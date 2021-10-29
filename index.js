9696b4a3876781f0d7b80625dee6479aa82c4272
NarraRineesh <rineeshkumar9@gmail.com>
2021-10-29 19:19:12 +0530

    language set

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { default: Axios } = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 const qs = require('qs')
-const FormData = require('form-data')
 var cors = require('cors')
+var handlebars = require('handlebars');
+var fs = require('fs');
 var bodyParser = require('body-parser')
-
 
 const app = express()
 app.use(cors())
@@ -16,10 +16,24 @@ app.use(express.static(__dirname));
 // get our app to use body parser 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get("/en", (req, res) => {
+    const data = require('./en.js')
+    readHTMLFile(__dirname + '/index.html', function(err, html) {
+        var template = handlebars.compile(html);
+        var replacements = data
+        var htmlToSend = template(replacements);
+        res.send(htmlToSend);
+    })
 });
-
+app.get("/sp", (req, res) => {
+    const data = require('./sp.js')
+    readHTMLFile(__dirname + '/index.html', function(err, html) {
+        var template = handlebars.compile(html);
+        var replacements = data
+        var htmlToSend = template(replacements);
+        res.send(htmlToSend);
+    })
+});
 app.get('/get-data/', async (req, res) => {
      try {
      const url = req.query.url
@@ -31,6 +45,18 @@ app.get('/get-data/', async (req, res) => {
           res.status(404).send(error);
      }       
  })
+ var readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+           callback(err); 
+           throw err;
+            
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
  app.listen(process.env.PORT || 5000, () => {
     console.log(`Application started and Listening on port ${process.env.PORT || 5000}`);
   });
