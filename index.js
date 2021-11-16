6efe7e258752b1eb6fdeba205f47158d736cfac2
NarraRineesh <62957541+NarraRineesh@users.noreply.github.com>
2021-11-16 13:11:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,120 +1,13 @@
-const { default: Axios } = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
-const qs = require('qs')
-var cors = require('cors')
+let nodemailer = require('nodemailer');
 var handlebars = require('handlebars');
 var fs = require('fs');
-var https = require('https')
-var bodyParser = require('body-parser')
-const HttpsProxyAgent = require("https-proxy-agent");
-const httpsAgent = new HttpsProxyAgent({ host: "45.94.47.108", port: "8152", auth: "hfcewizn:w8hkja84jwem" })
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-
-app.get("/", (req, res) => {
-    app.use(express.static(__dirname));
-    const data = require('./en.js')
-    readHTMLFile(__dirname + '/index.html', function (err, html) {
-        var template = handlebars.compile(html);
-        var replacements = data
-        var htmlToSend = template(replacements);
-        res.send(htmlToSend);
-    })
-});
-app.get("/en", (req, res) => {
-
-    const data = require('./en.js')
-    readHTMLFile(__dirname + '/index.html', function (err, html) {
-        var template = handlebars.compile(html);
-        var replacements = data
-        var htmlToSend = template(replacements);
-        res.send(htmlToSend);
-    })
-});
-app.get("/sp", (req, res) => {
-    const data = require('./sp.js')
-    readHTMLFile(__dirname + '/index.html', function (err, html) {
-        var template = handlebars.compile(html);
-        var replacements = data
-        var htmlToSend = template(replacements);
-        res.send(htmlToSend);
-    })
-});
-app.get('/get-data/', async (req, res) => {
-    try {
-        const url = req.query.url
-        const data = await tiktokdownload(url);
-        console.log(data);
-        res.status(200).send(data);
-
-    } catch (error) {
-        res.status(404).send(error);
-    }
-})
-
-
-app.post("/api/download", async (request, response) => {
-    console.log("request coming in...", request.body.url);
-
-    try {
-        const links = await instaReel(request.body.url);
-        console.log(links);
-        if (links.is_video == true) {
-            response.json({ downloadLink: links.video_url });
-        } else if (links.is_video == false) {
-            response.json({ downloadLink: links.display_url });
-
-        } else {
-            response.json({ error: "The link you have entered is invalid. " });
-        }
-    } catch (err) {
-        console.log(err);
-        response.json({
-            error: "There is a problem with the link you have provided."
-        });
-    }
-});
-
-async function instaReel(URL) {
-    console.log("called!");
-    const { data } = await Axios.get(https://cors-anywhere.herokuapp.com/+URL, {
-        headers: {
-            accept: "*/*",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,en-IN;q=0.7",
-            "user-agent":
-                "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36",
-        },
-    }).catch(function (error) {
-        if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        };
-    });
-    let $ = cheerio.load(data);
-    let script = $("script").eq(4).html();
-    console.log(JSON.parse(/window\._sharedData = (.+);/g.exec(script)[1]));
-    let {
-        entry_data: {
-            PostPage: {
-                [0]: {
-                    graphql: {
-                        shortcode_media: { display_url, video_url, is_video },
-                    },
-                },
-            },
-        },
-    } = JSON.parse(/window\._sharedData = (.+);/g.exec(script)[1]);
-    return { display_url, video_url, is_video };
-}
+const app = express();
+const port = 3000;
+app.use(cors());
 var readHTMLFile = function (path, callback) {
     fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
         if (err) {
@@ -127,62 +20,44 @@ var readHTMLFile = function (path, callback) {
         }
     });
 };
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Application started and Listening on port ${process.env.PORT || 5000}`);
-});
 
+let mailerConfig = {
+    host: "smtp.office365.com",
+    secureConnection: true,
+    port: 587,
+    auth: {
+        user: "contact@daycoresolutions.com",
+        pass: "daycore123"
+    }
+};
+let transporter = nodemailer.createTransport(mailerConfig);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-function tiktokdownload(url) {
-    return new Promise((resolve, reject) => {
-        Axios.get('https://ttdownloader.com/')
-            .then((data) => {
-                const $ = cheerio.load(data.data)
-                const cookie = data.headers['set-cookie'].join('')
-                const dataPost = {
-                    url: url,
-                    format: '',
-                    token: $('#token').attr('value')
-                }
-                // return console.log(cookie);
-                Axios({
-                    method: 'POST',
-                    url: 'https://ttdownloader.com/req/',
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                        origin: 'https://ttdownloader.com',
-                        referer: 'https://ttdownloader.com/',
-                        cookie: cookie,
-                    },
-                    data: qs.stringify(dataPost)
-                }).then(({ data }) => {
-                    const $ = cheerio.load(data)
-                    const result = {
-                        nowm: $('#results-list > div:nth-child(2) > div.download > a')?.attr('href'),
-                        wm: $('#results-list > div:nth-child(3) > div.download > a')?.attr('href'),
-                        audio: $('#results-list > div:nth-child(4) > div.download > a').attr('href')
-                    }
-                    getData(result.wm)
-                    resolve(result);
-                })
-                    .catch(e => {
-                        reject({ status: false, message: 'error fetch data', e: e.message })
-                    })
-            })
-            .catch(e => {
-                reject({ status: false, message: 'error fetch data', e: e.message })
-            })
-    })
-}
-
-//use axios as you normally would, but specify httpsAgent in the config
-function getData(url) {
-    Axios.create({ httpsAgent });
-    Axios.get(url).then(function (res) {
-        // console.log(res.status);
-        // console.log(res.headers['x-client-ip']);
-        console.log(res.data._currentUrl);
-    }).catch(function (err) {
-        console.log(err);
+app.post('/send-mail', (req, res) => {
+    readHTMLFile('./index.html', function (err, html) {
+        var template = handlebars.compile(html);
+        var replacements = {
+            name: req.body.name,
+            email: req.body.email,
+            subject: req.body.subject,
+            message: req.body.message,
+        };
+        var htmlToSend = template(replacements);
+        let mailOptions = {
+            from: mailerConfig.auth.user,
+            to: 'rineeshkumar9@gmail.com,ajaytiyan@gmail.com,Kokanti.hemanth97@gmail.com',
+            subject: 'Contact Details',
+            html: htmlToSend
+        };
+        transporter.sendMail(mailOptions, function (error) {
+            if (error) {
+                res.status(200).send('Thanks for contact us! we will get back soon.')
+            } else {
+                res.status(404).send('Something went wrong! Try later.');
+            }
+        });
     });
-}
+});
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
